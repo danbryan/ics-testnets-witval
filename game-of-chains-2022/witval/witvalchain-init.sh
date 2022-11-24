4e6a3d91c53a7e22a788d853d8622ea82d155980
aleem1314 <aleem@vitwit.com>
2022-11-24 11:34:03 +0530

    wip

---
+++ game-of-chains-2022/witval/witvalchain-init.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+# Set up a witval service to join the witval chain.
+
+# Configuration
+# You should only have to modify the values in this block
+PRIV_VALIDATOR_KEY_FILE=~/priv_validator_key.json
+NODE_KEY_FILE=~/node_key.json
+NODE_HOME=~/.witvalchain
+NODE_MONIKER=witvalchain
+# ***
+
+# CHAIN_BINARY_URL='https://github.com/vitwit/ics-testnets/raw/main/game-of-chains-2022/witval/witvalchain'
+CHAIN_BINARY='witvalchain'
+CHAIN_ID=witval-1
+PERSISTENT_PEERS=""
+
+# Install go 1.19.2
+echo "Installing go..."
+rm go1.19.2.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.19.2.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.19.2.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+
+# Install chain binary
+echo "Installing witvalchain..."
+# mkdir -p $HOME/go/bin
+
+# # Download Linux amd64:
+# wget $CHAIN_BINARY_URL -O $HOME/go/bin/$CHAIN_BINARY
+# chmod +x $HOME/go/bin/$CHAIN_BINARY
+
+echo "Installing build-essential..."
+sudo apt install build-essential -y
+rm -rf witvalchain
+git clone https://github.com/vitwit/witval-chain/witval.git
+cd witvalchain
+git checkout v0.1.0
+make install
+
+export PATH=$PATH:$HOME/go/bin
+
+# Initialize home directory
+echo "Initializing $NODE_HOME..."
+rm -rf $NODE_HOME
+$CHAIN_BINARY init $NODE_MONIKER --chain-id $CHAIN_ID --home $NODE_HOME
+
+# Replace keys
+echo "Replacing keys and genesis file..."
+cp $PRIV_VALIDATOR_KEY_FILE $NODE_HOME/config/priv_validator_key.json
+cp $NODE_KEY_FILE $NODE_HOME/config/node_key.json
+
+# Reset state
+$CHAIN_BINARY tendermint unsafe-reset-all --home $NODE_HOME
+
+sudo rm /etc/systemd/system/$NODE_MONIKER.service
+sudo touch /etc/systemd/system/$NODE_MONIKER.service
+
+echo "[Unit]"                               | sudo tee /etc/systemd/system/$NODE_MONIKER.service
+echo "Description=witvalchain service"       | sudo tee /etc/systemd/system/$NODE_MONIKER.service -a
+echo "After=network-online.target"          | sudo tee /etc/systemd/system/$NODE_MONIKER.service -a
+echo ""                                     | sudo tee /etc/systemd/system/$NODE_MONIKER.service -a
+echo "[Service]"                            | sudo tee /etc/systemd/system/$NODE_MONIKER.service -a
+echo "User=$USER"                            | sudo tee /etc/systemd/system/$NODE_MONIKER.service -a
+echo "ExecStart=$HOME/go/bin/$CHAIN_BINARY start --x-crisis-skip-assert-invariants --home $NODE_HOME --p2p.persistent_peers $PERSISTENT_PEERS" | sudo tee /etc/systemd/system/$NODE_MONIKER.service -a
+echo "Restart=always"                       | sudo tee /etc/systemd/system/$NODE_MONIKER.service -a
+echo "RestartSec=3"                         | sudo tee /etc/systemd/system/$NODE_MONIKER.service -a
+echo "LimitNOFILE=4096"                     | sudo tee /etc/systemd/system/$NODE_MONIKER.service -a
+echo ""                                     | sudo tee /etc/systemd/system/$NODE_MONIKER.service -a
+echo "[Install]"                            | sudo tee /etc/systemd/system/$NODE_MONIKER.service -a
+echo "WantedBy=multi-user.target"           | sudo tee /etc/systemd/system/$NODE_MONIKER.service -a
+
+# Start service
+echo "Starting $NODE_MONIKER.service..."
+sudo systemctl daemon-reload
+
+# Add go and gaiad to the path
+echo "Setting up paths for go..."
+echo "export PATH=$PATH:/usr/local/go/bin" >> .profile
+
+echo "***********************"
+echo "After you have updated the genesis file, start the witvalchain service:"
+echo "sudo systemctl enable $NODE_MONIKER.service"
+echo "sudo systemctl start $NODE_MONIKER.service"
+echo "And follow the log with:"
+echo "journalctl -fu $NODE_MONIKER.service"
+echo "***********************"
